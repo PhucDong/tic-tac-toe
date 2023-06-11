f5d9405418740b4bf3246e6688fd9ccf1147692b
Phuc <dongvuminhphuc@gmail.com>
2023-06-11 22:05:40 +0700

    Add History feature and some comments

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Board from "./Board";
+import History from "./History";
 
 function Game() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState(null);
-  const [counter, setCounter] = useState(1);
+  // const [counter, setCounter] = useState(1);
+  const [historyList, setHistoryList] = useState([]);
+  const [turn, setTurn] = useState(0);
 
   // Declaring a Winner
   useEffect(() => {
@@ -34,7 +37,6 @@ function Game() {
     ];
 
     for (let i = 0; i < lines.length; i++) {
-      // i: 0, i < 8
       const [a, b, c] = lines[i];
       if (
         squares[a] &&
@@ -54,22 +56,31 @@ function Game() {
       return;
     }
 
+    // When players click on available spot, don't run the system
     if (squares[i]) return;
 
     // "Your code here";
     squares.map((square, index) => {
       if (index === i) {
         squares[i] = "X";
+        historyList[turn] = "Player's turn";
         setXIsNext(false);
       }
       return null;
     });
 
     const newSquares = [...squares];
+    const newHistoryList = [...historyList];
+    let newTurn = turn + 1;
+    // console.log(75, newTurn);
+    // console.log(76, historyList);
+    // console.log(77, newHistoryList);
 
-    // setSquares(squares);
+    setTurn(newTurn);
     setSquares(newSquares);
-    const machineTurn = setTimeout(handleComputerTurn, 1000);
+    setHistoryList(newHistoryList);
+
+    const machineTurn = setTimeout(handleComputerTurn(newTurn), 1000);
 
     if (calculateWinner(squares)) {
       clearTimeout(machineTurn);
@@ -77,39 +88,37 @@ function Game() {
   };
 
   // Handle machine's turn
-  const handleComputerTurn = () => {
-    // generate a random number, 0-8
+  const handleComputerTurn = (newTurn) => {
     let randomNumber = Math.floor(Math.random() * 9);
-
-    if (counter === 9) {
-      setWinner("Draw");
-      return;
-    }
 
     // check if anyone already played that spot
     if (squares[randomNumber]) {
-      setCounter(counter + 1);
-      console.log(92, counter);
-
       handleComputerTurn();
     } else {
       squares.map((square, index) => {
         if (index === randomNumber) {
           squares[randomNumber] = "O";
+          // console.log(101, newTurn);
+          historyList[newTurn] = "Computer's turn";
           setXIsNext(true);
         }
         return null;
       });
 
       const newSquares = [...squares];
-
+      const newHistoryList = [...historyList];
+      // console.log(110, historyList);
+      // console.log(111, newHistoryList);
+      setTurn(newTurn + 1);
       setSquares(newSquares);
+      setHistoryList(newHistoryList);
     }
   };
 
   // Restart game
   const handleRestart = () => {
     setSquares(Array(9).fill(null));
+    setHistoryList([]);
     setWinner(null);
     setXIsNext(true);
   };
@@ -120,7 +129,10 @@ function Game() {
 
       <div className="game">
         <span className="player">Next player is: {xIsNext ? "X" : "O"}</span>
-        <Board squares={squares} handleClick={handleClick} />
+        <div className="board-history-container">
+          <Board squares={squares} handleClick={handleClick} />
+          <History historyList={historyList} />
+        </div>
       </div>
 
       <button onClick={handleRestart} className="restart-btn">
